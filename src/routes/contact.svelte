d9acdaacef808de46886b52e8793e09f32bb8c62
Pavel Zapletal <pavel.zaplet25@gmail.com>
2022-01-06 21:36:41 +0100

    fixing layout issues

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,15 +1,15 @@
-<main>
-	<div class="section">
-		<div class="max-width">
-			<div class="container">
-				<h1 class="headline">Contact me!</h1>
-				<a href="mailto:liamwalker@gmal.com" class="text">liamwalker@gmal.com</a>
-			</div>
+<div class="section">
+	<div class="max-width">
+		<div class="container">
+			<h1 class="headline">Contact me!</h1>
+			<a href="mailto:liamwalker@gmal.com" class="text">liamwalker@gmal.com</a>
 		</div>
 	</div>
-</main>
+</div>
 
 <style lang="scss">
+	@use '../styles/mixins';
+
 	.container {
 		display: flex;
 		flex-direction: column;
@@ -19,6 +19,12 @@
 
 		a {
 			color: inherit;
+		}
+	}
+
+	.section {
+		@include mixins.set-breakpoint('mobile') {
+			height: calc(100vh - var(--nav-ofst));
 		}
 	}
 </style>
